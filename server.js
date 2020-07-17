929500af13fdcc3d7d791f55a2fde36c882313e7
Ayla Dillis <aayladillis@gmail.com>
2020-07-16 22:28:56 -0700

    trying again

--- server.js
+++ server.js
@@ -1,30 +1,13 @@
-// dependencies
-// =============================================================
 var express = require("express");
-var fs = require("fs");
-var path = require("path");
-
-var db = [];
-
-// express app
-// =============================================================
 var app = express();
+var routes = require("routes");
+
 var PORT = process.env.PORT || 8080;
 
-//sets up the express app to handle data parsing
-app.use(express.urlencoded ({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes
-// =============================================================
-app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "notes.html"));
-});
-
-app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "./db.json"));
-});
+app.use(routes);
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
